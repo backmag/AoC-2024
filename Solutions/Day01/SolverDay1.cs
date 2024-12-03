62b3477523ae173b2fb7a9849e7632d13e9f68e8
Gustaf Backman <gustaf.backman@spp.se>
2024-12-03 22:44:55 +0100

    Day 1: Add C# impl & remove python

--- Solutions/Day01/SolverDay1.cs
+++ Solutions/Day01/SolverDay1.cs
@@ -10,15 +10,46 @@
         }
         public override BigInteger SolvePartOne()
         {
-            var lines = GetInput();
-            return BigInteger.Zero;
+            string[] lines = GetInput();
+            (List<int> l1, List<int> l2) = ParseLists(lines);
+
+            l1.Sort();
+            l2.Sort();
+
+            int result = l1.Zip(l2, (nbr1, nbr2) => Math.Abs(nbr1 - nbr2)).Sum();
+            return (BigInteger)result;
         }
 
         public override BigInteger SolvePartTwo()
         {
-            var lines = GetInput();
-            return BigInteger.Zero;
+            string[] lines = GetInput();
+            (List<int> l1, List<int> l2) = ParseLists(lines);
+
+            int result = l1.Select(nbr1 => l2.Count(nbr2 => nbr1 == nbr2) * nbr1).Sum();
+            return (BigInteger)result;
         }
+
+        public Tuple<List<int>, List<int>> ParseLists(string[] lines)
+        {
+            List<int> l1 = [];
+            List<int> l2 = [];
+            foreach (var line in lines)
+            {
+                var numbers = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                if (numbers.Length != 2)
+                {
+                    throw new Exception($"Couldn't parse input: {line}");
+                }
+                if (int.TryParse(numbers.First(), out int first)
+                    && int.TryParse(numbers.Last(), out int last))
+                {
+                    l1.Add(first);
+                    l2.Add(last);
+                }
+            }
+            return Tuple.Create(l1, l2);
+        }
+
         private string[] GetInput()
         {
             return _inputService.GetInput();
