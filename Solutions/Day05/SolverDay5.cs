45af3e43922d7582d7ba22d8e794ce295bb546bb
Gustaf Backman <gustaf.backman@spp.se>
2024-12-05 18:01:58 +0100

    Day 5: Quicker implementation with HashSet

--- Solutions/Day05/SolverDay5.cs
+++ Solutions/Day05/SolverDay5.cs
@@ -3,8 +3,6 @@
     public class SolverDay5 : Solver
     {
         private readonly InputService _inputService;
-        private readonly Dictionary<string, List<int>> _listCache = [];
-        private readonly Dictionary<string, bool> _boolCache = [];
         public SolverDay5(InputService inputService)
         {
             _inputService = inputService;
@@ -15,20 +13,15 @@
             var input = GetInput();
             var (orderingRules, updates) = ParseInput(input);
             var retVal = 0;
-            var nbrValidUpdates = 0;
 
             foreach (var update in updates)
             {
-                // extract only relevant rules
-                var relevantRules = orderingRules.Where(r => update.Any(u => u.Equals(r.Item1) || u.Equals(r.Item2)));
-                if (IsValidUpdate(update, relevantRules))
+                if (IsValidUpdate(update, orderingRules))
                 {
                     int midIdx = (update.Count - 1) / 2;
                     retVal += update[midIdx];
-                    nbrValidUpdates++;
                 }
             }
-            Console.WriteLine($"Found {nbrValidUpdates}. There are {updates.Count - nbrValidUpdates} invalid rows.");
             return retVal;
         }
 
@@ -37,104 +30,75 @@
             var input = GetInput();
             var (orderingRules, updates) = ParseInput(input);
             var retVal = 0;
-            int nbrFixes = 0;
 
             foreach (var update in updates)
             {
-                // extract only relevant rules
-                var relevantRules = orderingRules.Where(r => update.Any(u => u.Equals(r.Item1) || u.Equals(r.Item2)));
-                if (!IsValidUpdate(update, relevantRules))
+                if (!IsValidUpdate(update, orderingRules))
                 {
-                    Console.WriteLine($"Fix nbr {nbrFixes}");
-                    var newUpdate = FixUpdateList(update, relevantRules);
+                    var newUpdate = FixUpdateList(update, orderingRules);
                     int midIdx = (newUpdate.Count - 1) / 2;
                     retVal += newUpdate[midIdx];
-                    nbrFixes++;
                 }
             }
             return retVal;
         }
 
-        public bool IsValidUpdate(List<int> update, IEnumerable<Tuple<int, int>> rules)
+        public bool IsValidUpdate(List<int> updates, HashSet<(int first, int second)> rules)
         {
-            string cacheKey = GetHashKey(update, rules);
-            if (_boolCache.TryGetValue(cacheKey, out var value))
+            for (int firstIdx = 0; firstIdx < updates.Count; firstIdx++)
             {
-                return value;
-            }
-            for (int i = 0; i < update.Count; i++)
-            {
-                for (int j = i + 1; j < update.Count; j++)
+                for (int secondIdx = firstIdx + 1; secondIdx < updates.Count; secondIdx++)
                 {
-                    var firstPage = update[i];
-                    var secondPage = update[j];
-                    if (rules.Where(r => r.Item1 == secondPage && r.Item2 == firstPage).Any())
+                    if (rules.Contains((updates[secondIdx], updates[firstIdx])))
                     {
-                        _boolCache.Add(cacheKey, false);
                         return false;
                     }
                 }
             }
-            _boolCache.Add(cacheKey, true);
             return true;
         }
-        public string GetHashKey(List<int> update, IEnumerable<Tuple<int, int>> rules)
-        {
-            return String.Join(",", update) + "|" + String.Join(",", rules);
-        }
 
-        public List<int> FixUpdateList(List<int> update, IEnumerable<Tuple<int, int>> rules)
+        public List<int> FixUpdateList(List<int> update, IEnumerable<(int first, int second)> rules)
         {
-            // worked without caching, but took ~4 min
-            string cacheKey = GetHashKey(update, rules);
-            if (_listCache.TryGetValue(cacheKey, out var value))
-            {
-                return value;
-            }
-            List<int> retList = [];
+            HashSet<int> pagesToPlace = new(update);
+            List<int> newList = [];
 
-            for (int i = 0; i < update.Count; i++)
+            while (pagesToPlace.Count > 0)
             {
-                for (int j = i + 1; j < update.Count; j++)
+                foreach (var page in pagesToPlace)
                 {
-                    var firstPage = update[i];
-                    var secondPage = update[j];
-                    if (rules.Where(r => r.Item1 == secondPage && r.Item2 == firstPage).Any())
+                    if (!rules.Where(e => pagesToPlace.Contains(e.first) && e.second == page).Any())
                     {
-                        // switch places of the failing pages
-                        var newUpdate = new List<int>(update);
-                        newUpdate[i] = update[j];
-                        newUpdate[j] = update[i];
-                        if (IsValidUpdate(newUpdate, rules))
-                        {
-                            _listCache.Add(cacheKey, newUpdate);
-                            return newUpdate;
-                        }
-                        return FixUpdateList(newUpdate, rules);
+                        newList.Add(page);
+                        pagesToPlace.Remove(page);
+                        break;
                     }
                 }
             }
-            return retList;
+            return newList;
         }
 
-        public Tuple<List<Tuple<int, int>>, List<List<int>>> ParseInput(string[] input)
+        public (HashSet<(int first, int second)>, List<List<int>>) ParseInput(string[] input)
         {
-            var orderings = new List<Tuple<int, int>>();
+            var orderRules = new HashSet<(int first, int second)>();
             int idx = 0;
+            // read orderings
             while (input[idx] != "")
             {
                 var orderList = input[idx].Split('|').Select(e => int.Parse(e)).ToList();
-                orderings.Add(Tuple.Create(orderList[0], orderList[1]));
+                orderRules.Add((first: orderList[0], second: orderList[1]));
                 idx++;
             }
             idx++;
             var updates = new List<List<int>>();
+
+            // read update sequences
             while (idx < input.Length)
             {
                 updates.Add(input[idx].Split(",").Select(e => int.Parse(e)).ToList());
                 idx++;
             }
-            return Tuple.Create(orderings, updates);
+            return (orderRules, updates);
         }
 
         public string[] GetInput()
